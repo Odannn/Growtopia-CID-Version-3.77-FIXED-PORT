0b9a0b704a95c055296da6837d56df99fe75a2d7
ODAN PROJECT <98178345+Odannn@users.noreply.github.com>
2022-02-15 00:54:34 +0700

    Update corefunc.h

--- Create ID Version 3.77 + FIXED PORT/corefunc.h
+++ Create ID Version 3.77 + FIXED PORT/corefunc.h
@@ -55,7 +55,7 @@ public:
 	string currentWorld;
 	bool connect = false;
 	int timeFromWorldEnter = 0; // in 10mss...
-	string gameVersion = "3.78";
+	string gameVersion = "3.81";
 	int owner = -1;
 	string ownerUsername;
 	int localX = -1;
